295f58053385c856c226f55eab1f85edf70c87f7
Vlad Mundi <vladmundiciems@gmail.com>
2020-05-23 16:51:24 +0100

    Improve infrastructure

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  watch: false,
+  watch: true,
   watchOptions: {
     aggregateTimeout: 300,
     poll: 1000
@@ -22,5 +23,13 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'index.html') },
+        { from: path.resolve(__dirname, 'style.css') },
+      ],
+    }),
+  ],
 };
